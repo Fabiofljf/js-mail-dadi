9835a1a8a503033bfd1c7cdbf0f70a4557cec8e7
FabioLaForesta <fabiolaforesta@icloud.com>
2022-03-29 16:15:26 +0200

    finish - changes css

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,34 +1,39 @@
 //Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
 
-// PRIMO PASSAGGIO - creo una lista di chi può accedere, uso arrey
+//CREO PULSANTE PER GENERARE IL TUTTO
+document.getElementById('btndiVerifica').addEventListener('click', verifica)
 
-const Accesso = ['verdi@gmail.com', 'rossi@gmail.com', 'gialli@gmail.com']
+function verifica() {
 
-// VARIABILI DI RICHIAMO
-const listaAccesso = Accesso
+    // PRIMO PASSAGGIO - creo una lista di chi può accedere, uso arrey
+    const Accesso = ['verdi@gmail.com', 'rossi@gmail.com', 'gialli@gmail.com']
 
-// SECONDO PASSAGGIO - Chiedo all'utente la sua email
-const email = prompt('Digitare email')
+    // VARIABILI DI RICHIAMO (facoltativa)
+    const listaAccesso = Accesso
 
-// VARIABILI DI RICHIAMO
-const nomeUtente = email
+    // SECONDO PASSAGGIO - Chiedo all'utente la sua email
+    const email = document.getElementById('email').value
 
-//Variabile email non torvata (variabile sentinella) la imposto false perché ancora non ho nessun valore
-let elementoTrovato = false
+    // VARIABILI DI RICHIAMO (facoltativa)
+    const nomeUtente = email
+
+    //Variabile email non torvata (variabile sentinella) la imposto false perché ancora non ho nessun valore
+    let elementoTrovato = false
 
 
-// TERZO PASSAGGIO - Verifico l'accesso
-for (let i = 0; i < Accesso.length; i++) {
-    if (nomeUtente == listaAccesso[i]) {
-        elementoTrovato = true
+    // TERZO PASSAGGIO - Verifico l'accesso
+    for (let i = 0; i < Accesso.length; i++) {
+        if (nomeUtente == listaAccesso[i]) {
+            elementoTrovato = true
+        }
     }
-}
 
-//Quando trovo l'elementoTrovato vero stampo il messaggio
-if (elementoTrovato == true) {
-    console.log('Accesso consentito')
-} else {
-    console.log('Accesso negato')
+    //Quando trovo l'elementoTrovato vero stampo il messaggio
+    if (elementoTrovato == true) {
+        document.getElementById('risultatoAccesso').innerHTML = 'Accesso consentito'
+    } else {
+        document.getElementById('risultatoAccesso').innerHTML = 'Accesso negato'
+    }
 }
 
 
@@ -36,10 +41,14 @@ if (elementoTrovato == true) {
 
 // Genero numero random per giocatore
 const dadiUser = Math.floor(Math.random() * 7)
+
+// VARIABILI DI RICHIAMO (facoltativa)
 const giocataUser = dadiUser
 
 // Genero numero random per il computer
 const dadiPc = Math.floor(Math.random() * 7)
+
+// VARIABILI DI RICHIAMO (facoltativa)
 const giocataPc = dadiPc
 
 //Scrivo i rispettivi risultati sulla pagina
