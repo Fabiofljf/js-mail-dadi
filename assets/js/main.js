460590a09a043c4e3e16d8e3e858ad3078054138
FabioLaForesta <fabiolaforesta@icloud.com>
2022-03-29 15:24:20 +0200

    add plays

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,18 +7,32 @@ const Accesso = ['verdi@gmail.com', 'rossi@gmail.com', 'gialli@gmail.com']
 // VARIABILI DI RICHIAMO
 const listaAccesso = Accesso
 
-// Chiedo all'utente la sua email
+// SECONDO PASSAGGIO - Chiedo all'utente la sua email
 const email = prompt('Digitare email')
 
 // VARIABILI DI RICHIAMO
 const nomeUtente = email
 
-//Verifico l'accesso
+// TERZO PASSAGGIO - Verifico l'accesso
 for (let i = 0; i < Accesso.length; i++) {
     if (`${nomeUtente} == ${listaAccesso}`) {
         console.log('utente verificato')
     } else {
         console.log('accesso negato')
     }
-
 }
+
+
+// QUARTO PASSAGGIO - Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. 
+
+// Genero numero random per giocatore
+const dadiUser = Math.floor(Math.random() * 7)
+console.log(`${dadiUser}`)
+
+// Genero numero random per il computer
+const dadiPc = Math.floor(Math.random() * 7)
+console.log(`${dadiPc}`)
+
+//Scrivo i rispettivi risultati sulla pagina
+document.getElementById('giocataGiocatore').innerHTML = `${dadiUser}`
+document.getElementById('giocataComputer').innerHTML = `${dadiPc}`
